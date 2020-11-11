5428f91388773371f39e1e6c3ca32efa0044b37f
Shumail Mohyuddin <shumailmohyuddin@gmail.com>
2020-11-11 13:14:11 +0100

    make sorting work!

--- src/App.js
+++ src/App.js
@@ -6,21 +6,16 @@ import axios from "axios";
 
 function App() {
 
-  const [movies, setMovies] = useState([]);
-  const [searchResults, setSearchResults] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [filteredSearchResults, setFilteredSearchResults] = useState([]);
+  const [orignalSearchResults, setOriginalSearchResults] = useState([]);
   const [query, setQuery] = useState("");
-  const [dataByDate, setDataByDate] = useState([]);
-  const [dataByPopularity, setDataByPopularity] = useState([]);
-  // const [fullDate, setFullDate] = useState([]);
-  // const [moviePopularity, setMoviePopularity] = useState([]);
-  // const [sortTypeByDate, setSortTypeByDate] = useState('date');
-  // const [sortTypeByPopularity, setSortTypeByPopularity] = useState('popularity');
 
 
   useEffect(() => {
     const url = "https://api.themoviedb.org/3/movie/popular?api_key=bcf11cf802fb05846974d00acd973616&language=en-US&page=1";
     axios.get(url).then(response => {
-      setMovies(response.data.results)
+      setPopularMovies(response.data.results)
       // setFullDate(response.data.results.release_date)
       // setMoviePopularity(response.data.results.popularity)
 
@@ -34,39 +29,40 @@ function App() {
     setQuery(input);
     if (input.trim() === "") {
       // reset search results
-      setSearchResults([]);
+      setFilteredSearchResults([]);
+      setOriginalSearchResults([]);
       return;
     }
     const newurl = `https://api.themoviedb.org/3/search/movie?api_key=bcf11cf802fb05846974d00acd973616&language=en-US&query=${input}&page=1&include_adult=false`;
     axios.get(newurl).then(response => {
-      setSearchResults(response.data.results)
+      setOriginalSearchResults(response.data.results);
+      setFilteredSearchResults(response.data.results);
     });
     // console.log("searchone", searchResults);
-    
+
   }
 
-    const sortArrayByDate = () => {
-      // const url = "https://api.themoviedb.org/3/movie/popular?api_key=bcf11cf802fb05846974d00acd973616&language=en-US&page=1";
-      // axios.get(url).then(response => {
-      // const sortByDate = response.data.results;
-      const sortByDate = searchResults;
-      console.log("first",sortByDate)
-      let sorted = sortByDate.sort((a, b) => new Date(b.release_date.split('/').reverse().join("-")) - new Date(a.release_date.split('/').reverse().join("-")) );
-      console.log("second",sorted);
-      setDataByDate(sorted);
-    //})
-    };
-
-    const sortArrayByPopularity = () => {
-      const url = "https://api.themoviedb.org/3/movie/popular?api_key=bcf11cf802fb05846974d00acd973616&language=en-US&page=1";
-    axios.get(url).then(response => {
-      const sortByPopularity = response.data.results;
-      console.log("first",sortByPopularity.popularity)
-      let sorted = sortByPopularity.sort((a, b) => (b.popularity) -(a.popularity));
-      console.log("second",sorted);
-      setDataByPopularity(sorted);
-    })
-    };
+  const handleSorting = async (e) => {
+    // important to use "slice" because we need to get a "copy" of array fileredSearchResults, because Array.sort does "in-place" sorting and modifies actual array, but 
+    // we don't want to modify filteredSearchResults array directly. Need to use React way 
+    console.log('handle sorting', e);
+    if (e === 'date_desc') {
+      const x = filteredSearchResults.slice().sort((a, b) => new Date(b.release_date.split('/').reverse().join("-")) - new Date(a.release_date.split('/').reverse().join("-")));
+      setFilteredSearchResults(x);
+    }
+    if (e === 'date_asc') {
+      const x = filteredSearchResults.slice().sort((a, b) => new Date(a.release_date.split('/').reverse().join("-")) - new Date(b.release_date.split('/').reverse().join("-")));
+      setFilteredSearchResults(x);
+    }
+    if (e === 'rating_asc') {
+      const x = filteredSearchResults.slice().sort((a, b) => a.vote_average - b.vote_average);
+      setFilteredSearchResults(x);
+    }
+    if (e === 'rating_desc') {
+      const x = filteredSearchResults.slice().sort((a, b) => b.vote_average - a.vote_average);
+      setFilteredSearchResults(x);
+    }
+  }
 
   return (
     <div className="App">
@@ -83,37 +79,44 @@ function App() {
         />
       </form>
 
+      {/* show search results if available & sorting dropdown */}
+      {filteredSearchResults.length > 0 &&
+        <div>
+          <div className="dropdown">
+            <select onChange={(e) => handleSorting(e.target.value)}>
+              <option> Select</option>
+              <option value="date_desc">Date: Descending</option>
+              <option value="date_asc">Date: Ascending</option>
+              <option value="rating_asc">Rating: Ascending</option>
+              <option value="rating_desc">Rating: Descending</option>
+            </select>
+          </div>
+          <div className="search-results-title">Search Results</div>
+          <div className="card-list">
+            {filteredSearchResults.filter(movie => movie.poster_path).map(movie => {
+              return (
+                <MovieCards movie={movie} key={movie.id} />
+              )
+            })}
+          </div>
+        </div>
+      }
 
-      <div className="dropdown">
-      <select onClick={(e) => sortArrayByDate(e.target.value)} >
-        <option >Select</option>
-        <option  value="date" >By Date</option>
-        <option value="popularity" >By Popularity</option>
-      </select>
-      </div>
-      
+      {orignalSearchResults.length === 0 &&
+        <div>
+          <div className="popular-movie-title">Popular Movies</div>
+          <div className="card-list">
+            {/* Display Popular Movies, check against original search results - not filtered */}
+            {popularMovies.filter(movie => movie.poster_path).map(movie => {
+              return (
+                <MovieCards movie={movie} key={movie.id} />
+              )
+            })}
+          </div>
 
-      <div className="popular-movie-title">Popular Movie</div>
-      <div className="card-list">
-      {/* {console.log("releaseDate", releaseDate)} */}
-        {console.log("searchResult", searchResults)}
-        {searchResults.length > 0 && searchResults.filter(movie => movie.poster_path).map(movie => {
-            return (
-              <MovieCards movie={movie} key={movie.id} />
-            )
-        })}
+        </div>
 
-        {searchResults.length === 0 && movies.filter(movie => movie.poster_path).map(movie => {
-          return (
-            <MovieCards movie={movie} key={movie.id} />
-          )
-        })}
-        {/* {dataByDate.map(movie => {
-          return(
-            <MovieCards movie={movie} key={movie.id} />
-          )
-        })} */}
-      </div>
+      }
 
     </div>
   );
