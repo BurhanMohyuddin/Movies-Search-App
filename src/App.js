04bc9bc48b08a2c93aebe04aef940b19a99c7246
Burhan Mohyuddin <burhan365@gmail.com>
2020-11-14 13:48:55 +0500

    new date filter

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ function App() {
   const [filteredSearchResults, setFilteredSearchResults] = useState([]);
   const [orignalSearchResults, setOriginalSearchResults] = useState([]);
   const [query, setQuery] = useState("");
+  const [filterByDate, setfilterByDate] = useState({
+    firstDate: "",
+    lastDate: "",
+  });
 
   useEffect(() => {
-    const url =
-      "https://api.themoviedb.org/3/movie/popular?api_key=bcf11cf802fb05846974d00acd973616&language=en-US&page=1";
+    const url ="https://api.themoviedb.org/3/movie/popular?api_key=bcf11cf802fb05846974d00acd973616&language=en-US&page=1";
     axios.get(url).then((response) => {
       setPopularMovies(response.data.results);
-      // setFullDate(response.data.results.release_date)
-      // setMoviePopularity(response.data.results.popularity)
     });
   }, []);
 
@@ -34,7 +35,7 @@ function App() {
     const newurl = `https://api.themoviedb.org/3/search/movie?api_key=bcf11cf802fb05846974d00acd973616&language=en-US&query=${input}&page=1&include_adult=false`;
     axios.get(newurl).then((response) => {
       setOriginalSearchResults(response.data.results);
-      setFilteredSearchResults(response.data.results); 
+      setFilteredSearchResults(response.data.results);
     });
   };
 
@@ -43,11 +44,11 @@ function App() {
     // we don't want to modify filteredSearchResults array directly. Need to use React way
     console.log("handle sorting", e);
     if (e === "date_desc") {
-      const x = filteredSearchResults.slice().sort((a, b) =>new Date(b.release_date.split("/").reverse().join("-")) - new Date(a.release_date.split("/").reverse().join("-")));
+      const x = filteredSearchResults.slice().sort((a, b) => new Date(b.release_date.split("/").reverse().join("-")) - new Date(a.release_date.split("/").reverse().join("-")));
       setFilteredSearchResults(x);
     }
     if (e === "date_asc") {
-      const x = filteredSearchResults.slice().sort((a, b) =>new Date(a.release_date.split("/").reverse().join("-")) -new Date(b.release_date.split("/").reverse().join("-")));
+       const x = filteredSearchResults.slice().sort((a, b) => new Date(a.release_date.split("/").reverse().join("-")) - new Date(b.release_date.split("/").reverse().join("-")));
       setFilteredSearchResults(x);
     }
     if (e === "rating_asc") {
@@ -59,31 +60,25 @@ function App() {
       setFilteredSearchResults(x);
     }
   };
+
   
-  //to filter by date
-  let maxOffset = 30;
-  let thisYear = (new Date()).getFullYear();
-  let allYears = [];
-  for(let x = 0; x <= maxOffset; x++) {
-    allYears.push(thisYear - x)
-  }
-  const yearList = allYears.map((x) => {return(<option key={x}>{x}</option>)});
-  
-  
+
   const handleFiltering = async (e) => {
-    console.log("date filtering", e);
-    const x = filteredSearchResults.filter(m => m.release_date.split("-")[0] === e);
+    const value = e.target.value;
+    setfilterByDate({
+      ...filterByDate,
+      [e.target.name]: value,
+    });
+
+    console.log("first value", filterByDate.firstDate);
+    console.log("second value", filterByDate.lastDate);
+
+    
+  };
+
+  const handleFilteringByClick = () => {
+    const x = filteredSearchResults.filter(m => m.release_date.split("-")[0] > filterByDate.firstDate && m.release_date.split("-")[0] < filterByDate.lastDate);
     setFilteredSearchResults(x);
-    // filteredSearchResults.filter((filteredResult) => filteredResult.poster_path).map(filteredResult => {
-    //   let dates = filteredResult.release_date;
-    //   console.log("asdakdkasdhajkd",dates)
-    //    if(e === dates)
-    //      setFilteredSearchResults(filteredResult);
-    //     console.log("date filtering",filteredResult);
-    //   return <MovieCards movie={filteredResult} key={filteredResult.id} />;
-    // })
-
-
   }
   return (
     <div className="App">
@@ -104,19 +99,41 @@ function App() {
       {filteredSearchResults.length > 0 && (
         <div>
           <div className="dropdown">
-            <select onChange={(e) => handleSorting(e.target.value)} style={{borderRadius: '8px' }}>
+            <select
+              onChange={(e) => handleSorting(e.target.value)}
+              style={{ borderRadius: "8px" }}
+            >
               <option> Select</option>
               <option value="date_desc">Date: Descending</option>
               <option value="date_asc">Date: Ascending</option>
               <option value="rating_asc">Rating: Ascending</option>
               <option value="rating_desc">Rating: Descending</option>
             </select>
-            <select className="date-dropdown" onChange={(e) => handleFiltering(e.target.value)}>
-              <option>Select</option>
-              {yearList}
-            </select>
+            <div className="dateHandler">
+              <input
+                className="start-date"
+                type="text"
+                name="firstDate"
+                placeholder="i.e 2010"
+                size="4"
+                value={filterByDate.firstDate}
+                onChange={handleFiltering}
+              />
+              <strong className="textData"> TO </strong>
+              <input
+                className="last-date"
+                type="text"
+                name="lastDate"
+                placeholder="i.e 2020"
+                size="4"
+                value={filterByDate.lastDate}
+                onChange={handleFiltering}
+              />
+              <button onClick={handleFilteringByClick}> Search </button>
+            </div>
+            
           </div>
-    
+
           <div className="search-results-title">Search Results</div>
           <div className="card-list">
             {filteredSearchResults
